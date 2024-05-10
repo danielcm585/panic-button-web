406e5748254f90277a0cc2e50998752d73497e1e
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-05-10 15:47:39 +0700

    ci: fixed linter

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,7 +26,10 @@ export default function page() {
         <div className="py-20 px-6 flex flex-col gap-6 ">
             <h1 className="text-3xl font-bold">ADMIN</h1>
             {data.map((item, index) => (
-                <div className="w-full bg-gray-100 border-2 border-gray-200 rounded-lg p-4">
+                <div 
+                    key={`report-${index}`}
+                    className="w-full bg-gray-100 border-2 border-gray-200 rounded-lg p-4"
+                >
                     {item.slug === 'firefighter' ? (
                         <p className="font-semibold text-red-500 text-lg">Firefighter</p>
                     ) : item.slug === 'police' ? (
